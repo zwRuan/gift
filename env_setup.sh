f509880329b56561b9cd152df62166847d96aa22
zwRuan <1434800673@qq.com>
2026-04-14 21:51:06 +0800

    Init

--- env_setup.sh
+++ env_setup.sh
@@ -4,9 +4,9 @@ set -e
 # docker: runpod/pytorch:2.2.1-py3.10-cuda12.1.1-devel-ubuntu22.04
 curl -LsSf https://astral.sh/uv/install.sh | sh
 export PATH="$HOME/.local/bin:$PATH"
-uv venv uv_pwsft --python 3.12
+uv venv uv_gift --python 3.12
 
-source uv_pwsft/bin/activate
+source uv_gift/bin/activate
 
 uv pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu124
 uv pip install -r requirements.txt
